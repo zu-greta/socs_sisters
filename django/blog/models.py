5f985eea0fe7ff843dfdb2de64f4491bba061945
greta_zu <149711822+zu-greta@users.noreply.github.com>
2024-11-30 12:06:42 -0500

    django setup

--- django/blog/models.py
+++ django/blog/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
 # Create your models here.
+# testing
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
--- django/blog/views.py
+++ django/blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.utils import timezone
+from blog.models import Post
+
+# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+
+    return render(request, 'blog/posts_list.html', {
+        'posts': posts
+    })
