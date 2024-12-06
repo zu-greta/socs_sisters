c84aa2b24f33d2ed25c60cb91bd2ff6fe1ffd51a
AKey <keyu.wang@mail.mcgill.ca>
2024-12-05 19:25:34 -0500

    forgot to commit the frontend folder

---
+++ frontend_stuff/backend_js/register.js
@@ -0,0 +1,12 @@
+function redirectToLogin() {
+    // show popup is required fields are not filled
+
+
+    /* extra checks with backend for duplicate email is needed*/
+    
+
+
+
+    // alert('Registration successful! Redirecting...');
+    window.location.href = "../../database_stuff/register.php";
+}
